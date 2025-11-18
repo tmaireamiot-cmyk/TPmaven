910bc99803a5fbe846b31feae46757c7133c30d9
Tanguy Maire-Amiot <t.maireamiot@gmail.com>
2025-11-18 17:24:46 +0100

    partie 5

--- src/main/java/fr/esir/omd/ci/TaskManager.java
+++ src/main/java/fr/esir/omd/ci/TaskManager.java
@@ -3,18 +3,26 @@ package fr.esir.omd.ci;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class TaskManager {
     private List<Task> tasks;
+    private static final Logger logger = LoggerFactory.getLogger(TaskManager.class);
 
     public TaskManager() {
         tasks = new ArrayList<>();
+        
     }
 
     public void addTask(Task task) {
         tasks.add(task);
+        logger.info("Nouvelle tâche ajoutée {}", task.getTitle());
     }
 
     public List<Task> getTasks() {
+        logger.debug("Récupération de la liste des {} tâches", tasks.size());
         return tasks;
     }
 }
